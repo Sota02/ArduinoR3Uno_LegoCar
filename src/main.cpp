c51fd9a034c2539bb6ce3a5594cbde5a3ae2184f
Sota <bluceislee@gmail.com>
2019-09-24 15:47:28 +0200

    all methods are extracted from loop

--- src/main.cpp
+++ src/main.cpp
@@ -17,13 +17,21 @@ const int steerAngle = 8;
 const uint8_t L293D_ENable1Pin = 3;
 const uint8_t L293D_1APin = 5;
 const uint8_t L293D_2APin = 6;
-uint8_t ifGoingForward = 0; //0 is stop, 1 is go forward
-uint8_t ifGoingBackward = 0; //0 is stop, 1 is go backward
+uint8_t ifGoingBackward = 0; //0=stop, 1=go backward
 int drivingDirection = 0; //0=stop,1=forward,-1=backward
-int motorSpeed; //current motorspeed. any value between 0-255
+int motorSpeed; //current motorspeed. value is between 0-255
 const uint8_t initialMotorSpeed = 120; //initial speed when accel button is pressed
 //LED
 const uint8_t LEDPin = 13;
+uint8_t LEDstatus = 0; //0=off,1=on
+
+//Methods
+void steerWheel(char c);
+void stepOnGasPedal(char c);
+void stepOnGasPedal(char c);
+void stopMotor();
+void toggleLED();
+void rotateDCMotor();
 
 void setup() {  
     //serial communication to config HC-05
@@ -46,43 +54,7 @@ void setup() {
     //LED
     pinMode(LEDPin,OUTPUT); 
     digitalWrite(LEDPin,HIGH);
-    
-}
-
-void steerWheel(char x){
-  if(x=='l'){
-    if(servo1.read()-steerAngle >= initialAngle-limitAngle){
-      servo1.write(servo1.read()-steerAngle);
-    } 
-  }else if(x=='r'){
-    if(servo1.read()+steerAngle <= initialAngle+limitAngle){
-      servo1.write(servo1.read()+steerAngle);
-    }
-  }
-  Serial.print("angleServo:");    
-  Serial.println(servo1.read());
-}
-
-void stepOnGasPedal(char x){
-  if(x == 'f'){
-    if(drivingDirection!=1){
-      drivingDirection = 1;
-      motorSpeed = initialMotorSpeed;
-    }else{
-      if(motorSpeed<= 255){
-        motorSpeed+=20;
-      }
-    }
-  }else if(x == 'b'){
-    if(drivingDirection != -1){
-      drivingDirection = -1;
-      motorSpeed = initialMotorSpeed;
-    }else{
-      if(motorSpeed<= 255){
-        motorSpeed+=20;
-      }
-    }
-  }
+    LEDstatus = 1;
 }
 
 void loop()
@@ -101,30 +73,76 @@ void loop()
         }else if(receivedChar == 'b'){
           stepOnGasPedal('b');
         }else if(receivedChar == 's'){
-            digitalWrite(L293D_ENable1Pin,LOW);
-            motorSpeed = 0;
-            drivingDirection = 0;
+          stopMotor();
         }else if(receivedChar == 'p'){
-          digitalWrite(LEDPin,HIGH);
-        }else if(receivedChar == 'q'){
-          digitalWrite(LEDPin,LOW);
+          toggleLED();  
         }
     }
 
     //dc motor
-    if(drivingDirection==1){
+    rotateDCMotor();
+}
+
+void steerWheel(char c){
+  if(c=='l'){
+    if(servo1.read()-steerAngle >= initialAngle-limitAngle){
+      servo1.write(servo1.read()-steerAngle);
+    } 
+  }else if(c=='r'){
+    if(servo1.read()+steerAngle <= initialAngle+limitAngle){
+      servo1.write(servo1.read()+steerAngle);
+    }
+  }
+  Serial.print("angleServo:");    
+  Serial.println(servo1.read());
+}
+
+void stepOnGasPedal(char c){
+  if(c == 'f'){
+    if(drivingDirection!=1){
+      drivingDirection = 1;
+      motorSpeed = initialMotorSpeed;
+    }else{
+      if(motorSpeed<= 255){
+        motorSpeed+=20;
+      }
+    }
+  }else if(c == 'b'){
+    if(drivingDirection != -1){
+      drivingDirection = -1;
+      motorSpeed = initialMotorSpeed;
+    }else{
+      if(motorSpeed<= 255){
+        motorSpeed+=20;
+      }
+    }
+  }
+}
+
+void stopMotor(){
+  digitalWrite(L293D_ENable1Pin,LOW);
+  motorSpeed = 0;
+  drivingDirection = 0;
+}
+
+void rotateDCMotor(){
+  if(drivingDirection==1){
       analogWrite(L293D_ENable1Pin,motorSpeed);
       digitalWrite(L293D_1APin, HIGH);
       digitalWrite(L293D_2APin, LOW);
-    }else if(drivingDirection==-1){
+  }else if(drivingDirection==-1){
       analogWrite(L293D_ENable1Pin,motorSpeed);
       digitalWrite(L293D_1APin, LOW);
       digitalWrite(L293D_2APin, HIGH);
-    }
-        
-    //read input from Serial -> output to bluetooth
-    if (Serial.available()){
-       btport.write(Serial.read());
-    }
-    
+  }
+}
+
+void toggleLED(){
+  if(LEDstatus==0){
+    digitalWrite(LEDPin,HIGH);
+    LEDstatus=1;
+  }else{
+    digitalWrite(LEDPin,LOW);
+    LEDstatus=0;
+  }
 }
