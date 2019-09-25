cb0f1122cd2786cbe5f1417526364e4b5d827319
Sota <bluceislee@gmail.com>
2019-09-25 10:57:35 +0200

    serial console is refined

--- src/main.cpp
+++ src/main.cpp
@@ -2,54 +2,54 @@
 #include <SoftwareSerial.h>
 #include <Servo.h> 
 
-#define echoPin 2 // Echo Pin
-#define trigPin 4 // Trigger Pin
-double Duration = 0; //受信した間隔
-double Distance = 0; //距離
-int distanceCounter = 0;
-
-//bluetooth
-const byte btRxPin = 10;
-const byte btTxPin = 11;
+//bluetooth HC-05
+#define btRxPin 10
+#define btTxPin 11
 SoftwareSerial btport(btRxPin, btTxPin);
-char receivedChar; 
+char receivedChar;
 //servo motor
+#define servoPin 9
 Servo servo1;
-const uint8_t servoPin = 9;
 int initialAngle;
 const int limitAngle = 25;
 const int steerAngle = 8; 
 //dc motor
-const uint8_t L293D_ENable1Pin = 3;
-const uint8_t L293D_1APin = 5;
-const uint8_t L293D_2APin = 6;
-uint8_t ifGoingBackward = 0; //0=stop, 1=go backward
+#define L293D_ENable1Pin 3
+#define L293D_1APin 5
+#define L293D_2APin 6
 int drivingDirection = 0; //0=stop,1=forward,-1=backward
 int motorSpeed; //current motorspeed. value is between 0-255
 const uint8_t initialMotorSpeed = 120; //initial speed when accel button is pressed
+//ultra sonic distance sensor HC-SR04
+#define echoPin 2
+#define trigPin 4
+double Duration = 0; 
+double Distance = 0;
+int distanceCounter = 0;
 //LED
-const uint8_t LEDPin = 13;
+#define LEDPin 13
 uint8_t LEDstatus = 0; //0=off,1=on
 
 //Methods
+void showReceivedChar();
 void steerWheel(char c);
-void stepOnGasPedal(char c);
 void stepOnGasPedal(char c);
 void stopMotor();
 void toggleLED();
 void rotateDCMotor();
-void analyzeDistance();
+void measureDistance();
+
 
 void setup() {  
-    //serial communication to config HC-05
+    //serial communication to configure HC-05
     Serial.begin(9600);
-    Serial.println("You can enter AT commands:");
+    Serial.println("Hello! Robot car booted.");
     //bluetooth over SoftwareSerial
     btport.begin(38400); //38400 = default bluetooth communication rate 
     //servo motor
     pinMode(servoPin,OUTPUT); 
     servo1.attach(servoPin);
-    Serial.print("Servo angle inital value:");
+    Serial.print("Inital servo angle: ");
     Serial.println(servo1.read());  
     initialAngle = servo1.read();
     //dc motor
@@ -58,7 +58,7 @@ void setup() {
     pinMode(L293D_2APin,OUTPUT);
     digitalWrite(L293D_ENable1Pin,LOW);
     motorSpeed = initialMotorSpeed;
-    //distance censor
+    //ultrasonic distance censor
     pinMode( echoPin, INPUT );
     pinMode( trigPin, OUTPUT );
     //LED
@@ -69,31 +69,44 @@ void setup() {
 
 void loop()
 {
-    //receive char via bluetooth app. Then execute a action.
     if (btport.available()){
-        receivedChar = btport.read();
-        Serial.println(receivedChar);
-
+        //show a char in serial which received from bluetooth app
+        showReceivedChar();
+        
+        //execute a action
         if(receivedChar == 'l'){
+          Serial.println("steer left");
           steerWheel('l');
         }else if(receivedChar == 'r'){
+          Serial.println("steer right");
           steerWheel('r');
         }else if(receivedChar == 'f'){
+          Serial.println("go forward");
           stepOnGasPedal('f');
         }else if(receivedChar == 'b'){
+          Serial.println("go backward");
           stepOnGasPedal('b');
         }else if(receivedChar == 's'){
+          Serial.println("stop the car");
           stopMotor();
         }else if(receivedChar == 'p'){
+          Serial.println("put LED ON/OFF");
           toggleLED();  
         }
     }
 
     //dc motor
     rotateDCMotor();
-
     //distance sensor
-    analyzeDistance();
+    measureDistance();
+}
+
+void showReceivedChar(){
+  receivedChar = btport.read();
+  if(receivedChar!='0' && receivedChar!='o') {
+    Serial.print("Received char via Bluetooth: ");
+    Serial.println(receivedChar);
+  }
 }
 
 void steerWheel(char c){
@@ -138,6 +151,16 @@ void stopMotor(){
   drivingDirection = 0;
 }
 
+void toggleLED(){
+  if(LEDstatus==0){
+    digitalWrite(LEDPin,HIGH);
+    LEDstatus=1;
+  }else{
+    digitalWrite(LEDPin,LOW);
+    LEDstatus=0;
+  }
+}
+
 void rotateDCMotor(){
   if(drivingDirection==1){
       analogWrite(L293D_ENable1Pin,motorSpeed);
@@ -150,30 +173,27 @@ void rotateDCMotor(){
   }
 }
 
-void toggleLED(){
-  if(LEDstatus==0){
-    digitalWrite(LEDPin,HIGH);
-    LEDstatus=1;
-  }else{
-    digitalWrite(LEDPin,LOW);
-    LEDstatus=0;
-  }
-}
-
-void analyzeDistance(){
-  if(distanceCounter%80000==0) {
+void measureDistance(){
+  if(distanceCounter%20000==0) {
     digitalWrite(trigPin, LOW); 
     delayMicroseconds(2); 
-    digitalWrite( trigPin, HIGH ); //超音波を出力
-    delayMicroseconds( 10 ); //
+    digitalWrite( trigPin, HIGH ); //output ultrasonic sensor
+    delayMicroseconds(10);
     digitalWrite( trigPin, LOW );
-    Duration = pulseIn( echoPin, HIGH ); //センサからの入力
+    Duration = pulseIn( echoPin, HIGH ); //input from sensor = round trip time
     if (Duration > 0) {
-      Duration = Duration/2; //往復距離を半分にする
-      Distance = Duration*340*100/1000000; // 音速を340m/sに設定
-      Serial.print("Distance:");
+      //show distance in serial
+      Duration = Duration/2;
+      Distance = Duration*340*100/1000000; // define the speed of sonic as 340m/s
+      Serial.print("Distance from a object: ");
       Serial.print(Distance);
       Serial.println(" cm");
+
+      //stop the car if the distance is short
+      if(Distance < 20 && drivingDirection == 1){
+        stopMotor();
+        Serial.println("Sensor detected a object too near. stop.");
+      }
     }
   }
   distanceCounter+=1;
